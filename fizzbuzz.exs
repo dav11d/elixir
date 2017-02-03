f5385013993760b7cb07824838f79f132eb68bc6
David <koerce@gmail.com>
2017-02-03 19:45:30 +0000

    master

---
+++ fizzbuzz.exs
@@ -0,0 +1,12 @@
+fizzword = fn
+  (0, 0, _) -> "FizzBuzz"
+  (0, _, _) -> "Fizz"
+  (_, 0, _) -> "Buzz"
+  (_, _, n) -> n
+end
+
+fizzbuzz = fn (n) ->
+  fizzword.(rem(n, 3), rem(n, 5), n)
+end
+
+IO.inspect Enum.map(1..100, fizzbuzz)
